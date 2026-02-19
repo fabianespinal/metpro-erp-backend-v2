058f094d0aa05ecf3b8343a1ab215e694257c1ea
Samuel Fabian <sfabian@ramsinternationalgc.com>
2026-02-19 17:54:20 -0500

    Update

--- models/contact.py
+++ models/contact.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
+from sqlalchemy import Column, Integer, String, DateTime, func
 from database import Base
 
 class Contact(Base):
     __tablename__ = "contacts"
 
     id = Column(Integer, primary_key=True, index=True)
-    company_id = Column(Integer, ForeignKey("clients.id"), nullable=False)
+    company_id = Column(Integer, nullable=False)
     name = Column(String, nullable=False)
     email = Column(String, nullable=True)
     phone = Column(String, nullable=True)
